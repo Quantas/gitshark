1ea9a4f8d4d2e94ccdb57b146e9ee86e4c4f1324
Andrew Landsverk <dewdew@gmail.com>
2015-07-05 00:27:07 -0500

    log exceptions in the ExceptionAdvice

--- src/main/java/com/quantasnet/gitserver/git/ui/ExceptionAdvice.java
+++ src/main/java/com/quantasnet/gitserver/git/ui/ExceptionAdvice.java
@@ -1,5 +1,7 @@
 package com.quantasnet.gitserver.git.ui;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,6 +13,8 @@ import com.quantasnet.gitserver.git.exception.GitServerException;
 @ControllerAdvice("com.quantasnet.gitserver.git.ui")
 public class ExceptionAdvice {
 
+	private static final Logger LOG = LoggerFactory.getLogger(ExceptionAdvice.class);
+	
 	@ExceptionHandler({ GitServerException.class })
 	@ResponseStatus(HttpStatus.NOT_FOUND)
 	public String notFound() {
@@ -19,7 +23,8 @@ public class ExceptionAdvice {
 	
 	@ExceptionHandler({ GitServerErrorException.class })
 	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-	public String serverError() {
+	public String serverError(final GitServerErrorException exception) {
+		LOG.error("Error loading page", exception);
 		return "forward:/503";
 	}
 }
