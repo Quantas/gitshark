fe925a5c9521b07ba2d9f69bc31660824d9be305
Andrew Landsverk <dewdew@gmail.com>
2015-07-05 00:26:18 -0500

    fix page top issues

--- src/main/java/com/quantasnet/gitserver/Constants.java
+++ src/main/java/com/quantasnet/gitserver/Constants.java
@@ -40,6 +40,11 @@ public abstract class Constants {
 	
 	public static final String OS_USER_HOME = System.getProperty("user.home");
 	
+	
+	public static final String SUCCESS_STATUS = "successStatus";
+	
+	public static final String FAILURE_STATUS = "failureStatus";
+	
 	private Constants() {
 	}
 
