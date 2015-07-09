17a3b652e3d7f3446e4a6d9c5ffa9ee15457a2ac
Andrew Landsverk <dewdew@gmail.com>
2015-07-08 22:54:18 -0500

    repository creation from UI

--- src/main/java/com/quantasnet/gitserver/git/ui/RepoManageController.java
+++ src/main/java/com/quantasnet/gitserver/git/ui/RepoManageController.java
@@ -1,20 +1,29 @@
 package com.quantasnet.gitserver.git.ui;
 
+import com.quantasnet.gitserver.Constants;
+import com.quantasnet.gitserver.git.repo.GitRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.quantasnet.gitserver.git.exception.GitServerException;
 import com.quantasnet.gitserver.git.repo.FilesystemRepositoryService;
 import com.quantasnet.gitserver.user.User;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @RequestMapping("/repo")
 @Controller
 public class RepoManageController {
+
+	private static final String REPO_FORM = "repoForm";
 
 	@Autowired
 	private FilesystemRepositoryService repoService;
@@ -26,14 +35,50 @@ public class RepoManageController {
 	}
 	
 	@RequestMapping(value = "/create", method = RequestMethod.GET)
-	public String create() {
-		return "git/create";
+	public String create(final Model model) {
+		if (!model.containsAttribute(REPO_FORM)) {
+			model.addAttribute(REPO_FORM, new NewRepoForm());
+		}
+		return "git/repocreate";
 	}
 	
-	@RequestMapping(value = "/create/{repoName}", method = RequestMethod.POST)
-	public String createRepo(@AuthenticationPrincipal final User user, @PathVariable final String repoName) throws GitServerException {
-		repoService.createRepo(repoName, user.getUsername());
-		return "redirect:/repo/";
+	@RequestMapping(value = "/create", method = RequestMethod.POST)
+	public String createRepo(@AuthenticationPrincipal final User user, @Valid final NewRepoForm repoForm, final BindingResult bindingResult, final RedirectAttributes redirectAttributes) throws GitServerException {
+
+		if (!bindingResult.hasErrors()) {
+			for (final GitRepository repo : repoService.getRepositories(user.getUserName())) {
+				if (repo.getDisplayName().equals(repoForm.getRepoName())) {
+					bindingResult.rejectValue("repoName", "reponame.exists", "Repository already exists");
+					break;
+				}
+			}
+		}
+
+		if (bindingResult.hasErrors()) {
+			redirectAttributes.addFlashAttribute(BindingResult.MODEL_KEY_PREFIX + REPO_FORM, bindingResult);
+			redirectAttributes.addFlashAttribute(REPO_FORM, repoForm);
+			return "redirect:/repo/create";
+		}
+
+		repoService.createRepo(repoForm.getRepoName(), user.getUsername());
+
+		redirectAttributes.addFlashAttribute(Constants.SUCCESS_STATUS, "Repository created successfully.");
+		return "redirect:/repo/" + user.getUserName() + '/' + repoForm.getRepoName();
 	}
-	
+
+	private static class NewRepoForm {
+
+		@Size(min = 1, max = 200, message = "Repository name must be shorter than 200 characters.")
+		@Pattern(regexp = "^[A-Za-z0-9-]+$", message = "Repository name must not contain special characters or whitespace.")
+		private String repoName;
+
+		public String getRepoName() {
+			return repoName;
+		}
+
+		public void setRepoName(String repoName) {
+			this.repoName = repoName;
+		}
+	}
+
 }
