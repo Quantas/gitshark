3310f14d718c2bf7e7d9803f37b4878146c685d0
Andrew Landsverk <dewdew@gmail.com>
2015-07-16 22:32:17 -0500

    clean up whitespace

--- src/main/resources/static/js/branchchange.js
+++ src/main/resources/static/js/branchchange.js
@@ -6,36 +6,36 @@ function getParameterByName(name) {
 }
 
 if (!String.prototype.endsWith) {
-	String.prototype.endsWith = function(pattern) {
-		var d = this.length - pattern.length;
-		return d >= 0 && this.lastIndexOf(pattern) === d;
-	};
+    String.prototype.endsWith = function (pattern) {
+        var d = this.length - pattern.length;
+        return d >= 0 && this.lastIndexOf(pattern) === d;
+    };
 }
 
 var ogBranch;
 
-$(document).ready(function() {
-	var $dropdown = $('#branchDropdown');
-	ogBranch = $dropdown.val();
+$(document).ready(function () {
+    var $dropdown = $('#branchDropdown');
+    ogBranch = $dropdown.val();
 
-	$dropdown.on('change', function() {
-		if (this.value !== "") {
-			var path = window.location.pathname;
-			var isFile = getParameterByName('file');
+    $dropdown.on('change', function () {
+        if (this.value !== "") {
+            var path = window.location.pathname;
+            var isFile = getParameterByName('file');
 
-			if (path.endsWith('/tree')) {
-				var newPath = path + '/' + this.value;
-				if (isFile === 'true') {
-					newPath += '?file=true';
-				}
-				window.location.href = newPath;
-			} else {
-				var newPathWithBranch = path.replace('/tree/' + ogBranch, '/tree/' + this.value);
-				if (isFile === 'true') {
-					newPathWithBranch += '?file=true';
-				}
-				window.location.href = newPathWithBranch;
-			}
-		}
-	});
+            if (path.endsWith('/tree')) {
+                var newPath = path + '/' + this.value;
+                if (isFile === 'true') {
+                    newPath += '?file=true';
+                }
+                window.location.href = newPath;
+            } else {
+                var newPathWithBranch = path.replace('/tree/' + ogBranch, '/tree/' + this.value);
+                if (isFile === 'true') {
+                    newPathWithBranch += '?file=true';
+                }
+                window.location.href = newPathWithBranch;
+            }
+        }
+    });
 });
