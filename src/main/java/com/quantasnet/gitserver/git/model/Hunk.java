07cb160323f720549b85158a1db100a730f56903
Andrew Landsverk <dewdew@gmail.com>
2015-07-18 16:32:45 -0500

    update commit diff display
    split hunks into lines
    use table hover for better UI display
    coloring needs work...

--- src/main/java/com/quantasnet/gitserver/git/model/Hunk.java
+++ src/main/java/com/quantasnet/gitserver/git/model/Hunk.java
@@ -2,6 +2,10 @@ package com.quantasnet.gitserver.git.model;
 
 import java.io.Serializable;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.google.common.html.HtmlEscapers;
 
 public class Hunk implements Serializable {
 
@@ -36,6 +40,13 @@ public class Hunk implements Serializable {
 	public String getContents() {
 		return contents;
 	}
+
+	public List<HunkLine> getContentsLines() {
+		return Arrays.stream(
+			contents.split("\n"))
+			.map(this::buildLine)
+			.collect(Collectors.toList());
+	}
 	
 	public int getAdditions() {
 		return additions;
@@ -43,5 +54,44 @@ public class Hunk implements Serializable {
 	
 	public int getRemovals() {
 		return removals;
+	}
+
+	private HunkLine buildLine(final String line) {
+		final boolean add = line.startsWith("+");
+		final boolean delete = line.startsWith("-");
+
+		String newLine = line;
+		if (add || delete) {
+			newLine = line.substring(1);
+		}
+
+		newLine = HtmlEscapers.htmlEscaper().escape(newLine)
+			.replaceAll("\\t", "&nbsp;&nbsp;&nbsp;&nbsp;");
+
+		return new HunkLine(add, delete, newLine);
+	}
+
+	private class HunkLine {
+		private final boolean add;
+		private final boolean delete;
+		private final String text;
+
+		public HunkLine(final boolean add, final boolean delete, final String text) {
+			this.add = add;
+			this.delete = delete;
+			this.text = text;
+		}
+
+		public boolean isAdd() {
+			return add;
+		}
+
+		public boolean isDelete() {
+			return delete;
+		}
+
+		public String getText() {
+			return text;
+		}
 	}
 }
