94dabad65f00790a6d4d411f2bb19a849be8534d
Andrew Landsverk <dewdew@gmail.com>
2015-07-18 23:52:25 -0500

    handle 405 errors

--- src/main/java/com/quantasnet/gitserver/config/MvcConfig.java
+++ src/main/java/com/quantasnet/gitserver/config/MvcConfig.java
@@ -27,40 +27,42 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
 	private RepositoryResolver repositoryResolver;
 	
 	@Override
-    public void addViewControllers(final ViewControllerRegistry registry) {
-        super.addViewControllers(registry);
-        registry.addViewController("/login").setViewName("login");
-        registry.addViewController("/401").setViewName("error/401");
-        registry.addViewController("/403").setViewName("error/403");
-        registry.addViewController("/404").setViewName("error/404");
-        registry.addViewController("/503").setViewName("error/503");
-    }
+	public void addViewControllers(final ViewControllerRegistry registry) {
+		super.addViewControllers(registry);
+		registry.addViewController("/login").setViewName("login");
+		registry.addViewController("/401").setViewName("error/401");
+		registry.addViewController("/403").setViewName("error/403");
+		registry.addViewController("/404").setViewName("error/404");
+		registry.addViewController("/405").setViewName("error/404");
+		registry.addViewController("/503").setViewName("error/503");
+	}
 	
-    /**
-     * See for details: https://github.com/spring-projects/spring-boot/issues/2774
-     *  
-     * @param springSecurityFilterChain
-     * @return
-     */
-    @Bean
-    public FilterRegistrationBean getSpringSecurityFilterChainBindedToError(
-            @Qualifier("springSecurityFilterChain") Filter springSecurityFilterChain) {
+	/**
+	 * See for details: https://github.com/spring-projects/spring-boot/issues/2774
+	 *
+	 * @param springSecurityFilterChain
+	 * @return
+	 */
+	@Bean
+	public FilterRegistrationBean getSpringSecurityFilterChainBindedToError(
+			@Qualifier("springSecurityFilterChain") Filter springSecurityFilterChain) {
 
-        final FilterRegistrationBean registration = new FilterRegistrationBean();
-        registration.setFilter(springSecurityFilterChain);
-        registration.setDispatcherTypes(EnumSet.allOf(DispatcherType.class));
-        return registration;
-    }
-	
-    @Bean
-    public EmbeddedServletContainerCustomizer containerCustomizer() {
-        return container -> container.addErrorPages(
-        		new ErrorPage(HttpStatus.UNAUTHORIZED, "/401"),
-        		new ErrorPage(HttpStatus.FORBIDDEN, "/403"),
-                new ErrorPage(HttpStatus.NOT_FOUND, "/404"),
-                new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/503"));
-    }
-    
+		final FilterRegistrationBean registration = new FilterRegistrationBean();
+		registration.setFilter(springSecurityFilterChain);
+		registration.setDispatcherTypes(EnumSet.allOf(DispatcherType.class));
+		return registration;
+	}
+
+	@Bean
+	public EmbeddedServletContainerCustomizer containerCustomizer() {
+		return container -> container.addErrorPages(
+				new ErrorPage(HttpStatus.UNAUTHORIZED, "/401"),
+				new ErrorPage(HttpStatus.FORBIDDEN, "/403"),
+				new ErrorPage(HttpStatus.NOT_FOUND, "/404"),
+				new ErrorPage(HttpStatus.METHOD_NOT_ALLOWED, "/405"),
+				new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/503"));
+	}
+
 	@Override
 	public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> argumentResolvers) {
 		super.addArgumentResolvers(argumentResolvers);
