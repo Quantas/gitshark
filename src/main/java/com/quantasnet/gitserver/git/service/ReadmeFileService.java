c62c4ed4c17b4c2c45d5320e15b63473fabc1dcc
Andrew Landsverk <dewdew@gmail.com>
2015-07-25 21:51:25 -0500

    more readme cleanup
    bring back pegdown for Markdown
    custom styles for some readme sections
    show title for Asciidoc readies

--- src/main/java/com/quantasnet/gitserver/git/service/ReadmeFileService.java
+++ src/main/java/com/quantasnet/gitserver/git/service/ReadmeFileService.java
@@ -6,11 +6,13 @@ import java.util.List;
 
 import org.asciidoctor.Asciidoctor;
 import org.asciidoctor.Asciidoctor.Factory;
+import org.asciidoctor.AttributesBuilder;
 import org.asciidoctor.Options;
 import org.asciidoctor.OptionsBuilder;
 import org.asciidoctor.SafeMode;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Repository;
+import org.pegdown.PegDownProcessor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,7 +30,9 @@ public class ReadmeFileService {
 
 	private static final Logger LOG = LoggerFactory.getLogger(ReadmeFileService.class);
 
-	private static final List<String> README_NAMES = Arrays.asList("readme.md", "readme.markdown", "readme.adoc", "readme.ad");
+	private static final List<String> ASCIIDOC_NAMES = Arrays.asList("readme.adoc", "readme.ad");
+
+	private static final List<String> MARKDOWN_NAMES = Arrays.asList("readme.md", "readme.markdown");
 
 	private final Asciidoctor asciidoctor = Factory.create();
 
@@ -37,7 +41,6 @@ public class ReadmeFileService {
 	
 	@Cacheable(cacheNames = RepoCacheService.ALL_READMES, key = "{ #repo.fullDisplayName, #branch }")
 	public ReadmeFile resolveReadMeFile(final GitRepository repo, final Repository db, final String branch) throws GitServerException {
-		LOG.info("Cache Miss - Readme File - {} - {}", repo.getFullDisplayName(), branch);
 		return resolveReadMeFile(repo, db, branch, repoUtils.getFiles(repo, db, branch, "", false));
 	}
 	
@@ -48,9 +51,12 @@ public class ReadmeFileService {
 			if (!files.isEmpty()) {
 				for (final RepoFile file : files) {
 					final String fileName = file.getName();
-					if (README_NAMES.contains(fileName.toLowerCase())) {
+					if (ASCIIDOC_NAMES.contains(fileName.toLowerCase())) {
 						final String readme = new String(repoUtils.getFileContents(db, ObjectId.fromString(file.getObjectId())));
 						return new ReadmeFile(fileName, renderAsciiDoc(readme));
+					} else if (MARKDOWN_NAMES.contains(fileName.toLowerCase())) {
+						final String readme = new String(repoUtils.getFileContents(db, ObjectId.fromString(file.getObjectId())));
+						return new ReadmeFile(fileName, renderMarkdown(readme));
 					}
 				}
 			}
@@ -62,10 +68,15 @@ public class ReadmeFileService {
 	
 	private String renderAsciiDoc(final String originalText) {
 		final Options options = OptionsBuilder
-				.options()
-				.safe(SafeMode.SERVER)
-				.backend("xhtml5")
-				.get();
+			.options()
+			.safe(SafeMode.SERVER)
+			.backend("xhtml5")
+			.attributes(AttributesBuilder.attributes().showTitle(true))
+			.get();
 		return asciidoctor.render(originalText, options);
+	}
+
+	private String renderMarkdown(final String originalText) {
+		return new PegDownProcessor().markdownToHtml(originalText);
 	}
 }
