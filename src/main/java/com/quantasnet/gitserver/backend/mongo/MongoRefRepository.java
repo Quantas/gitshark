7512d1fd228cff2f864e37f5d4c0a6e231c4262e
Andrew Landsverk <dewdew@gmail.com>
2015-08-19 17:13:27 -0500

    clean up

--- src/main/java/com/quantasnet/gitserver/backend/mongo/MongoRefRepository.java
+++ src/main/java/com/quantasnet/gitserver/backend/mongo/MongoRefRepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 interface MongoRefRepository extends MongoRepository<MongoRef, String> {
-
 	List<MongoRef> findByRepoId(String repoId);
 	MongoRef findByRepoIdAndName(String repoId, String name);
 }
