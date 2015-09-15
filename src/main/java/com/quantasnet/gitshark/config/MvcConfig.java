6025c34cbf1d19e903c4d739a33e35072269ede5
Andrew Landsverk <dewdew@gmail.com>
2015-09-15 12:40:05 -0400

    oops from last commit

--- src/main/java/com/quantasnet/gitshark/config/MvcConfig.java
+++ src/main/java/com/quantasnet/gitshark/config/MvcConfig.java
@@ -40,7 +40,7 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
 		super.addViewControllers(registry);
 		registry.addViewController("/").setViewName("redirect:/repo");
 		registry.addViewController("/login").setViewName("login");
-		// registry.addViewController("/401").setViewName("error/401");
+		registry.addViewController("/401").setViewName("error/401");
 		registry.addViewController("/403").setViewName("error/403");
 		registry.addViewController("/404").setViewName("error/404");
 		registry.addViewController("/405").setViewName("error/404");
@@ -66,7 +66,7 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
 	@Bean
 	public EmbeddedServletContainerCustomizer containerCustomizer() {
 		return container -> container.addErrorPages(
-				new ErrorPage(HttpStatus.UNAUTHORIZED, "/401"),
+				// new ErrorPage(HttpStatus.UNAUTHORIZED, "/401"),
 				new ErrorPage(HttpStatus.FORBIDDEN, "/403"),
 				new ErrorPage(HttpStatus.NOT_FOUND, "/404"),
 				new ErrorPage(HttpStatus.METHOD_NOT_ALLOWED, "/405"),
