b7d26243d3d9ded8768cdbdd102bc2f3b1724e60
Andrew Landsverk <dewdew@gmail.com>
2015-09-15 12:35:42 -0400

    heroku test

--- src/main/java/com/quantasnet/gitshark/config/MvcConfig.java
+++ src/main/java/com/quantasnet/gitshark/config/MvcConfig.java
@@ -40,7 +40,7 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
 		super.addViewControllers(registry);
 		registry.addViewController("/").setViewName("redirect:/repo");
 		registry.addViewController("/login").setViewName("login");
-		registry.addViewController("/401").setViewName("error/401");
+		// registry.addViewController("/401").setViewName("error/401");
 		registry.addViewController("/403").setViewName("error/403");
 		registry.addViewController("/404").setViewName("error/404");
 		registry.addViewController("/405").setViewName("error/404");
